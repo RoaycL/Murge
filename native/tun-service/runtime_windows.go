2115eff590377b04e6fdd2b2bd8bd8afd4ee9541
luoyinfeng <1515357383@qq.com>
2026-09-08 20:51:42 +0800

    fix: make provider cache writer cross-platform

--- native/tun-service/runtime_windows.go
+++ native/tun-service/runtime_windows.go
@@ -667,29 +667,6 @@ func hashFile(path string) (string, error) {
 	return digest, err
 }
 
-func writePrivateFile(path string, data []byte) error {
-	temporary := path + ".tmp"
-	_ = os.Remove(temporary)
-	file, err := os.OpenFile(temporary, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0600)
-	if err != nil {
-		return err
-	}
-	if _, err = file.Write(data); err == nil {
-		err = file.Sync()
-	}
-	closeErr := file.Close()
-	if err != nil {
-		_ = os.Remove(temporary)
-		return err
-	}
-	if closeErr != nil {
-		_ = os.Remove(temporary)
-		return closeErr
-	}
-	_ = os.Remove(path)
-	return os.Rename(temporary, path)
-}
-
 func safeWindowsEnvironment() []string {
 	keys := []string{"SystemRoot", "WINDIR", "TEMP", "TMP"}
 	result := make([]string, 0, len(keys))
